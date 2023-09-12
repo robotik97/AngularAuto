d73a2fa28d102577cd5cd4e3ceea36d92863882f
unknown <edhar97@gmail.com>
2023-09-12 23:50:16 +0300

    feat: add new functionality to Auto options

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -25,45 +25,62 @@ export class CarComponent implements OnInit {
     };
     this.options = ['abs', 'esp', 'navigation system'];
   }
-  carSelect(carName: string){
-    if (carName=='Honda'){
-    this.name = 'Honda';
-    this.model = 'CR-V';
-    this.speed = 190;
-    this.colors = {
-      car: 'Black',
-      salon: 'White',
-      wheels: 'Silver',
-    };
-    this.options = ['abs', 'esp', 'navigation system']
-} else if (carName=='Audi') {
-    this.name = 'Audi';
-    this.model = 'Q8';
-    this.speed = 260;
-    this.colors = {
-      car: 'Black',
-      salon: 'White',
-      wheels: 'Silver',
-    };
-    this.options = ['abs', 'esp', 'navigation system'];
-  } else {
-    this.name = 'BMW';
-    this.model = 'M5';
-    this.speed = 190;
-    this.colors = {
-      car: 'Black',
-      salon: 'White',
-      wheels: 'Silver',
-    };
-    this.options = ['abs', 'esp', 'navigation system'];
+  carSelect(carName: string) {
+    if (carName == 'Honda') {
+      this.name = 'Honda';
+      this.model = 'CR-V';
+      this.speed = 190;
+      this.colors = {
+        car: 'Black',
+        salon: 'White',
+        wheels: 'Silver',
+      };
+      this.options = [
+        'abs',
+        'esp',
+        'navigation system',
+        'Automatic transmission',
+      ];
+    } else if (carName == 'Audi') {
+      this.name = 'Audi';
+      this.model = 'Q8';
+      this.speed = 260;
+      this.colors = {
+        car: 'Red',
+        salon: 'Black',
+        wheels: 'Black',
+      };
+      this.options = ['abs', 'esp', 'navigation system'];
+    } else {
+      this.name = 'BMW';
+      this.model = 'M5';
+      this.speed = 280;
+      this.colors = {
+        car: 'Black',
+        salon: 'Black',
+        wheels: 'Black',
+      };
+      this.options = ['abs', 'esp', 'navigation system', 'sport engine'];
+    }
   }
+
+  addNewOption(option: string) {
+    this.options.unshift(option);
+    return false;
+  }
+
+  deleteOption(option:string) {
+    for (let i = 0; i < this.options.length; i++) {
+      if (this.options[i] == option) {
+        this.options.splice(i, 1);
+        break;
+      }
+    }
   }
 }
- 
-
 
 interface Colors {
   car: string;
   salon: string;
-  wheels: string
+  wheels: string;
 }
