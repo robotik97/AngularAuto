6d1b452333705c9cbd3d84f3bfc70459321a2387
unknown <edhar97@gmail.com>
2023-09-14 22:02:19 +0300

    feat: add the ability to edit cars characteristics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
@@ -12,7 +13,8 @@ import { WheelComponent } from './components/wheel/wheel.component';
     WheelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
